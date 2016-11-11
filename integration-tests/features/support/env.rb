8cf6739d5871b8377db9ac76400e530be8609ca5
Lucas Machado <lucas.dpmachado@gmail.com>
2016-11-11 19:32:04 -0200

    Remove unnecessary files * Add Android manifest files, and TestServer.apk

--- integration-tests/features/support/env.rb
+++
@@ -1,7 +0,0 @@
-require 'calabash-android/color_helper'
-require 'calabash-android/operations'
-
-World(Calabash::Android::ColorHelper)
-World(Calabash::Android::Operations)
-
-ENV['TEST_APP_PATH'] = test_server_path(ENV['APP_PATH'])
--- integration-tests/features/support/helpers.rb
+++
@@ -1,7 +0,0 @@
-def ensure_app_installed
-  unless $app_installed
-    reinstall_apps
-  end
-
-  $app_installed = true
-end
